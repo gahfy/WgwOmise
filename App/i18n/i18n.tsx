26a612bf66e993253520d174aab5e2a71b73531c
gahfy <g.herfray@gahfy.io>
2023-06-27 17:16:30 +0700

    Fixing localization

--- App/i18n/i18n.tsx
+++ App/i18n/i18n.tsx
@@ -1,12 +1,17 @@
-import I18n from 'react-native-i18n';
+import { I18n } from 'i18n-js';
+import { findBestLanguageTag } from "react-native-localize";
 import en from './locales/en';
 import fr from './locales/fr'
+
+const translations = {
+  en: en,
+  fr: fr
+}
+
+const i18n = new I18n(translations);
+const bestLanguage = findBestLanguageTag(["en", "fr"]);
+console.log(bestLanguage)
+i18n.defaultLocale = "en"
+i18n.locale = bestLanguage?.languageTag ?? "en";
  
-I18n.fallbacks = true;
- 
-I18n.translations = {
-  en,
-  fr
-};
- 
-export default I18n;
+export default i18n;
